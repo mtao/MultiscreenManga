8b9f70e92d0f76252a2230b80210fb84c126d740
Andrew Wang <andrew@umbrant.com>
2012-12-30 21:55:05 -0800

    Add config entries for supported volume formats

--- src/configuration.cpp
+++ src/configuration.cpp
@@ -4,11 +4,16 @@
 
 ConfigurationHidden::ConfigurationHidden()
 {
-  // Read in Qt's reported supported image formats
-  QList<QByteArray> formatsBytes = QImageReader::supportedImageFormats();
-  for (auto && format: formatsBytes) {
-    supportedImageFormats.push_back(QString(format));
-  }
+    // Read in Qt's reported supported image formats
+    QList<QByteArray> formatsBytes = QImageReader::supportedImageFormats();
+    for (auto && format: formatsBytes) {
+        supportedImageFormats.push_back(QString(format));
+    }
+    // Set our supported volume formats
+    // TODO(umbrant): try to avoid hardcoding this
+    supportedVolumeFormats.push_back(QString("zip"));
+    supportedVolumeFormats.push_back(QString("rar"));
+    supportedVolumeFormats.push_back(QString("pdf"));
 }
 
 ConfigurationHidden & ConfigurationHidden::getInstance() {
@@ -21,5 +26,9 @@ ConfigurationHidden & ConfigurationHidden::getInstance() {
 Configuration::Configuration(): config(ConfigurationHidden::getInstance()) {}
 
 const QList<QString> & Configuration::getSupportedImageFormats() const {
-  return config.supportedImageFormats;
+    return config.supportedImageFormats;
+}
+
+const QList<QString> & Configuration::getSupportedVolumeFormats() const {
+    return config.supportedVolumeFormats;
 }
