b098dab69dd3b93348d5418be31227ec8bd56c99
Andrew Wang <andrew@umbrant.com>
2012-12-31 09:24:42 -0800

    Refactor out an openVolume method, some rename/cleanups

--- include/mainwindow.h
+++ include/mainwindow.h
@@ -6,6 +6,7 @@
 #include <memory>
 #include <mutex>
 #include <set>
+#include <stack>
 #include "mangavolume.h"
 #include "renderwidget.h"
 class QDockWidget;
@@ -13,25 +14,28 @@ class QDockWidget;
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-    
+
 public:
     MainWindow(QWidget *parent = 0);
     ~MainWindow();
-    private:
+
+private:
     QDockWidget * m_control;
     int m_page_num;
-    std::shared_ptr<const MangaVolume> m_volume;
+    std::shared_ptr<const MangaVolume> m_root_volume;
     std::mutex m_renderer_mutex;
 
-    private:
+    std::shared_ptr<const MangaVolume> openVolume(const QString & filename);
     //Don't need need to be smart because I'm taking care of this myself
     std::set< std::unique_ptr<RenderWidgetResource> > m_renderwidgets;
+    std::shared_ptr<Configuration> config;
 
-    protected:
+protected:
     void setFocus();
-    public slots:
-    void openFile();
-    void openFile(const QString & filepath);
+
+public slots:
+    void openRootVolume();
+    void openRootVolume(const QString & filepath);
     void nextPage();
     void previousPage();
     void changePage(int index);
@@ -41,8 +45,8 @@ public:
     void keyPressEvent(QKeyEvent *);
 
 
-    signals:
-    void newMangaVolume(std::shared_ptr<const MangaVolume> volume);
+signals:
+    void newRootMangaVolume(std::shared_ptr<const MangaVolume> volume);
     void newPage(uint page);
     void wantNewPage();
     void wantPrevPage();
